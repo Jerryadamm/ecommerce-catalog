68bac30d5d1c44fd5950f000b2a68261b8c3911d
Jerryadamm <103100361+Jerryadamm@users.noreply.github.com>
2022-12-04 23:22:16 +0700

    Add files via upload

---
+++ menView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/mencasual.png"
+    height="250px"
+    width="250px">>
+    
+    <HelloWorld msg="Mens Casual Slim Fit"/>
+    <p>
+      men's clothing
+    </p>
+<div>
+  <p>
+    great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.
+  </p>
+  <button>
+  buy now
+</button>
+</div>
+  </div>
+ 
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ womensView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/womenjaket.png"
+    height="300px"
+    width="300px">>
+    
+    <HelloWorld msg="Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket"/>
+    <p>
+      women's clothing
+    </p>
+    <div>
+  <p>
+  100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON
+  </p>
+  <button>
+  buy now
+</button>
+</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
